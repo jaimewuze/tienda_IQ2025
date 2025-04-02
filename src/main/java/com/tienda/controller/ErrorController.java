1825b395c5088fc0ea1e1a964092a1e4013a2bca
jaimewuze <jaimewu12345@gmail.com>
2025-04-02 16:58:18 -0600

    Semana 10

---
+++ src/main/java/com/tienda/controller/ErrorController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
+ */
+package com.tienda.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ControllerAdvice
+public class ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error/403";
+            } else if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/404";
+            }
+        }
+        return "error"; // Para otros casos generales
+    }
+}
